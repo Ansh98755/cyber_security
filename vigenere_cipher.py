5673175c1effc1f5c1b11a7657cf45b46026928e
Ansh98755 <ayushmehendiratta3390@gmail.com>
2025-04-17 13:13:54 +0530

    Successfully implemented the vigenere cipher

---
+++ vigenere_cipher.py
@@ -0,0 +1,63 @@
+def generate_key(msg, key):
+    key = list(key)
+    if len(msg) == len(key):
+        return key
+    else:
+        for i in range(len(msg) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+def encrypt_vigenere(msg, key):
+    encrypted_text = []
+    key = generate_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
+        else:
+            encrypted_char = char
+        encrypted_text.append(encrypted_char)
+    return "".join(encrypted_text)
+
+def decrypt_vigenere(msg, key):
+    decrypted_text = []
+    key = generate_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+        elif char.islower():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
+        else:
+            decrypted_char = char
+        decrypted_text.append(decrypted_char)
+    return "".join(decrypted_text)
+
+def main():
+    while True:
+        print("\n==== Vigenère Cipher Menu ====")
+        print("1. Encrypt")
+        print("2. Decrypt")
+        print("3. Exit")
+        choice = input("Enter your choice (1/2/3): ")
+
+        if choice == '1':
+            text = input("Enter the text to encrypt: ")
+            key = input("Enter the key: ")
+            encrypted = encrypt_vigenere(text, key)
+            print("Encrypted Text:", encrypted)
+        elif choice == '2':
+            text = input("Enter the text to decrypt: ")
+            key = input("Enter the key: ")
+            decrypted = decrypt_vigenere(text, key)
+            print("Decrypted Text:", decrypted)
+        elif choice == '3':
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice! Please select 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
